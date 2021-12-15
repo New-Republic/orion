b6b81afcaef88021d284448c7a8d64565c125e70
Baku Altybaev <altybaevbaku15@gmail.com>
2021-12-15 20:27:15 +0600

    Главный стр обновлен

--- code/src/pages/Home/HomeServices/Homeservices.jsx
+++ code/src/pages/Home/HomeServices/Homeservices.jsx
@@ -1,36 +1,16 @@
-import React from 'react'
+import React from "react";
 import "./HomeServices.scss";
-import HomeservicesTitle from './HomeservicesTitle';
-
 
 const HomeServices = (props) => {
-    return (
-      
-     
-           <div className="card">
-          
-             <div className="card__inner">
-               <img className="card__img" src={props.cardImg} alt="" />
-                 <h3 className="card__title">{props.cardTitle}</h3>
-                  <p className="card__descr">{props.cardDescription}</p>
-          </div>
-          
-         </div>
-        
-     
-  
-        
-          
-    );
-  };
-  
-
-  
+  return (
+    <div className="card">
+      <div className="card__inner">
+        <img className="card__inner-img" src={props.cardImg} alt="" />
+        <h3 className="card__inner-title">{props.cardTitle}</h3>
+        <p className="card__inner-descr">{props.cardDescription}</p>
+      </div>
+    </div>
+  );
+};
 
 export default HomeServices;
-
-
-
-
-
-
