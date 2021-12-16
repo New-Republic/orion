3f1b187bc7961850b06e99b2ba631421d0449fbc
Baku Altybaev <altybaevbaku15@gmail.com>
2021-12-16 21:33:33 +0600

    Update: Footer and home
    Добавлены - react-router-hash-link, semantic-ui-css, semantic-ui-react.
    Изменены - страницы Home и Footer.

--- code/src/components/Footer/Contacts/Contacts.jsx
+++ code/src/components/Footer/Contacts/Contacts.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Contacts() {
   return (
     <div className="contacts">
-      <div className="contacts__logo">
+      {/* <div className="contacts__logo">
         <span>Orion</span>
-      </div>
+      </div> */}
 
       <a href="tel:+996707113322">+996(707) 11 33 22</a>
       <address>Kyrgyzstan, Bishkek</address>
