4555f3f98db23e1a91a9ac5d1eca411245e83999
Baku Altybaev <altybaevbaku15@gmail.com>
2021-12-17 19:31:29 +0600

    Update footer

--- code/src/pages/About/Aboutus/Aboutus.jsx
+++ code/src/pages/About/Aboutus/Aboutus.jsx
@@ -3,18 +3,27 @@ import "./aboutus.scss";
 function Aboutus() {
   return (
     <div className="about">
-     <div className="aboutus">
-     <div className="aboutus__title">
-     <h3>О нас</h3>
-             <p>Консалтинговая компания ORION одна из ведущих консалтинговых компаний в сфере управленческого консалтинга, оптимизации процессов и маркетинга на рынке Кыргызстана и стран СНГ. Компания предоставляет широкий спектр консалтинговых услуг и успешно работает на рынке с 2008 года.<br></br>
-             Сейчас ORION — это сложившаяся команда профессионалов с богатым опытом работы в сфере стратегии, менеджмента, маркетинга, продаж,  бизнес моделирования, управления персоналом.</p>
-     </div>
-             
-             <img src="https://lh3.googleusercontent.com/proxy/zpVt0JkU31s4foI6qu7v59XADTWUhbf-iqDAQFRke_ncHxiH7Q6_BdwwVREt7xteBYxPFq2dPMb5vjv_lWWiX5hLxKxgHpvNDZN8rQfuUGdOURjOlQJkZVNKmVVOspyCvg6B1M1g"/>
-         
-     </div>
+      <div className="aboutus">
+        <div className="aboutus__title">
+          <h3>О нас</h3>
+          <p>
+            Консалтинговая компания ORION одна из ведущих консалтинговых
+            компаний в сфере управленческого консалтинга, оптимизации процессов
+            и маркетинга на рынке Кыргызстана и стран СНГ. Компания
+            предоставляет широкий спектр консалтинговых услуг и успешно работает
+            на рынке с 2008 года.<br></br>
+            Сейчас ORION — это сложившаяся команда профессионалов с богатым
+            опытом работы в сфере стратегии, менеджмента, маркетинга, продаж,
+            бизнес моделирования, управления персоналом.
+          </p>
+        </div>
+
+        <img
+          src="https://lh3.googleusercontent.com/proxy/zpVt0JkU31s4foI6qu7v59XADTWUhbf-iqDAQFRke_ncHxiH7Q6_BdwwVREt7xteBYxPFq2dPMb5vjv_lWWiX5hLxKxgHpvNDZN8rQfuUGdOURjOlQJkZVNKmVVOspyCvg6B1M1g"
+          alt="img"
+        />
+      </div>
     </div>
-    
   );
 }
 
